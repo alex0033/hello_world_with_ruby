397f58b0fbe9832d08a5faffe8566daa670b77f7
alex0033 <chanchanmen0807@icloud.com>
2020-11-06 14:59:53 +0900

    can connect next to home

---
+++ app/controllers/basic_pages_controller.rb
@@ -0,0 +1,8 @@
+class BasicPagesController
+    def home
+    end
+
+    def next
+        @next_string = "This is a next item."
+    end
+end
--- srv.rb
+++ srv.rb
@@ -2,11 +2,19 @@ require 'webrick'
 
 op = {
     BindAdress: "127.0.1",
-    Port: 10080,
-    DocumentRoot: "app/views/home.html.erb"
+    Port: 3000,
+    DocumentRoot: "."
 }
 
 WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
 s = WEBrick::HTTPServer.new(op)
+
+# 以下、URLと表示ファイルの個別設定
+s.mount('/', WEBrick::HTTPServlet::CGIHandler, 'home.rb')
+s.mount('/next', WEBrick::HTTPServlet::CGIHandler, 'next.rb')
+
+# 以下、URLと表示ファイルの個別設定
+s.mount('/', WEBrick::HTTPServlet::FileHandler, 'app/views/basic_pages/home.html.erb')
+s.mount('/next', WEBrick::HTTPServlet::FileHandler, 'app/views/basic_pages/next.html.erb')
 
 s.start
